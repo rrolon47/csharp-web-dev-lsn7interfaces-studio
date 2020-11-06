d0f9d4e8eb3cff83ef8d0893465c8ad93ea5685a
Rachel <rachel.rolon@gmail.com>
2020-11-05 19:10:59 -0600

    I messed up and lost all my progress tryng to do the studio on chapter 12

---
+++ SpinningDiscs/BaseDisk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    class abstract BaseDisk
+    {
+        public string Name { get; set; }
+        public string Capacity { get; set; }
+        public List<string> Content { get; set; }
+        public string DiskType { get; set; }
+
+
+
+    }
+}
---
+++ SpinningDiscs/IOpticalDisk.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    interface IOpticalDisk
+    {
+        void SpinDisk();
+        void ReadData();
+
+    }
+}
